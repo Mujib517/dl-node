4f68bb5eff17657bf34019031f93804ea1dc2503
Mujib517 <Mujib517@gmail.com>
2018-07-02 08:56:00 +0530

    Basic Authentication

--- index.js
+++ index.js
@@ -17,4 +17,28 @@ mongoose.connect("mongodb://localhost/dl-products", () => console.log("DB Connec
 app.use(bodyParser.json());
 
 app.use('/', defaultRouter);
+
+//middleware
+
+function authenticate(req, res, next) {
+
+  let base64String = req.headers["authorization"].replace("Basic ", "");
+  let decodedString = new Buffer(base64String, 'base64').toString(); // Ta-da
+
+  let tokens = decodedString.split(":");
+
+  let username = tokens[0];
+  let password = tokens[1];
+
+  if (username === "admin" && password === "password") next();
+  else {
+    res.status(401);
+    res.send("Unauthorized");
+  }
+}
+
+
+app.use(authenticate);
+
+//private
 app.use('/api/products', productRouter);
