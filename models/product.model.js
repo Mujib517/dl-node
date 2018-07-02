9711bd865fab684ea08b8a14188cbd493f6d2f9b
Mujib517 <Mujib517@gmail.com>
2018-07-02 08:33:16 +0530

    Custom Validator

--- models/product.model.js
+++ models/product.model.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
+function priceValidator(value) {
+  return value >= 100
+}
+//minlength: 3, maxlength: 10 
+
 module.exports = mongoose.model("Product", {
-  brand: { type: String, required: true },
-  model: { type: String, required: true },
-  price: { type: Number },
+  brand: { type: String, required: [true, "Brand is required"] },
+  model: { type: String, required: [true, "Model is mandatory"] },
+  price: {
+    type: Number, validate: { validator: priceValidator }
+  },
   inStock: { type: Boolean, default: false },
   lastUpdated: { type: Date, default: Date.now }
 });
