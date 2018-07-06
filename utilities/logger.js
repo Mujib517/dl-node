0edbce00ad41eca6a0754347c641de9d09d54b3e
Mujib517 <Mujib517@gmail.com>
2018-07-06 08:40:51 +0530

    Bunyan

--- utilities/logger.js
+++ utilities/logger.js
@@ -2,8 +2,18 @@ const bunyan = require("bunyan");
 const logger = bunyan.createLogger({
   name: 'dl-products',
   streams: [{
-    path: '/logs/application.log'
-  }]
+    level: 30,
+    path: 'logs/application.log'
+  },
+  {
+    level: 40,
+    path: 'logs/warnings.log'
+  },
+  {
+    level: 50,
+    path: 'logs/error.log'
+  }
+  ]
 });
 
 module.exports = logger;
