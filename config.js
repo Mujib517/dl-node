3fa2197ef327797016f678215d7d889c6913dfc7
mujib <mujib517@gmail.com>
2021-02-17 16:29:18 +0530

    Change mongdb con str

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   jwtPassword: 'secret',
-  conStr: "mongodb://admin:admin123@ds159344.mlab.com:59344/dl-products"
+  conStr: "mongodb+srv://admin:admin@cluster0.qcdda.mongodb.net/dl-products"
   //conStr: "mongodb://localhost/dl-products"
 };
 //DDD
